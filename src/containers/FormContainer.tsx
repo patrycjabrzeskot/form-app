486019172307507f770fd7302da51db2b317383e
Patrycja Brzeskot <patrycja.brzeskot97@gmail.com>
2020-07-22 11:33:50 +0200

    validation added

--- src/containers/FormContainer.tsx
+++ src/containers/FormContainer.tsx
@@ -1,10 +1,13 @@
 import { Card, CardContent, Container } from "@material-ui/core";
 import { ThemeContext } from "contexts/theme";
-import React, { useContext, useReducer } from "react";
+import React, { useContext, useReducer, useState } from "react";
 import Title from "../components/helpers/Title";
 import CheckBoxQuestion from "../components/inputs/CheckBox";
 import RadioQuestion from "../components/inputs/Radio";
 import TextFieldQuestion from "../components/inputs/TextField";
+import StyledButton from "../components/inputs/StyledButton";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
 
 const initialState = {
   gender: "",
@@ -37,7 +40,7 @@ function formReducer(prevState = initialState, action) {
     case formActionTypes.updateGender:
       return {
         ...prevState,
-        name: action.payload,
+        gender: action.payload,
       };
     case formActionTypes.updateAge:
       return {
@@ -113,6 +116,7 @@ function formReducer(prevState = initialState, action) {
 }
 const FormContainer: React.FC = () => {
   const theme = useContext(ThemeContext);
+
   const [
     {
       gender,
@@ -129,6 +133,25 @@ const FormContainer: React.FC = () => {
     dispach,
   ] = useReducer(formReducer, initialState);
 
+  const FormSchema = Yup.object().shape({
+    gender: Yup.string().required("To pole jest wymagane"),
+    age: Yup.string().required("To pole jest wymagane"),
+    isInterested: Yup.string().required("To pole jest wymagane"),
+    whichSport: Yup.array()
+      .required("To pole jest wymagane")
+      .min(2, "Zaznacz co najmniej jedną opcję"),
+    howOften: Yup.string().required("To pole jest wymagane"),
+    howLong: Yup.string().required("To pole jest wymagane"),
+    places: Yup.array()
+      .required("To pole jest wymagane")
+      .min(2, "Zaznacz co najmniej jedną opcję"),
+    mostImportant: Yup.array()
+      .required("To pole jest wymagane")
+      .min(2, "Zaznacz co najmniej jedną opcję"),
+    injury: Yup.string().required("To pole jest wymagane"),
+    competition: Yup.string().required("To pole jest wymagane"),
+  });
+
   return (
     <Container>
       <Card
@@ -137,178 +160,240 @@ const FormContainer: React.FC = () => {
       >
         <CardContent>
           <h3 style={{ color: theme.theme.text }}>Wypełnij krótką ankietę </h3>
-          <form noValidate autoComplete="off">
-            <Title number={1} title="Zaznacz swoją płeć" />
-            <RadioQuestion
-              options={["Kobieta", "Mężczyzna"]}
-              name="gender"
-              handleChange={(gender: string) =>
-                dispach({
-                  type: formActionTypes.updateGender,
-                  payload: gender,
-                })
-              }
-            />
-            <Title number={2} title="Ile masz lat?" />
-            <TextFieldQuestion
-              type="number"
-              name="age"
-              value={age}
-              handleChange={(age: string) =>
-                dispach({
-                  type: formActionTypes.updateAge,
-                  payload: age,
-                })
-              }
-            />
-            <Title number={3} title="Czy interesujesz się sportem?" />
-            <RadioQuestion
-              options={[
-                "Nie",
-                "Tak, uprawiam sport",
-                "Tak, jestem kibicem",
-                "Tak, kibicuję i uprawiam sport",
-              ]}
-              name="isInterested"
-              handleChange={(isInterested: string) =>
-                dispach({
-                  type: formActionTypes.updateIsInterested,
-                  payload: isInterested,
-                })
-              }
-            />
-            <Title
-              number={4}
-              title="Które z wymienionych sportów uprawiasz najczęściej?"
-            />
-            <CheckBoxQuestion
-              //whichSport
-              options={[
-                "piłka nożna",
-                "bieganie",
-                "siatkówka",
-                "lekkoatletyka",
-                "piłka ręczna",
-                "koszykówka",
-                "gimnastyka artystyczna",
-                "narciarstwo",
-                "tenis",
-                "pływanie",
-                "łyżwiarstwo",
-                "taniec",
-              ]}
-              name="whichSport"
-              value={whichSport}
-              handleChange={(newSport: string) => {
-                dispach({
-                  type: formActionTypes.updateWhichSport,
-                  payload: newSport,
-                });
-              }}
-            />
-            <Title number={5} title="Jak często uprawiasz sport?" />
-            <RadioQuestion
-              options={[
-                "1 raz w miesiącu",
-                "2 razy w miesiącu",
-                "Raz w tygodniu",
-                "Kilka razy w tygodniu",
-                "Codziennie",
-              ]}
-              name="howOften"
-              handleChange={(howOften: string) =>
-                dispach({
-                  type: formActionTypes.updateHowOften,
-                  payload: howOften,
-                })
-              }
-            />
-            <Title number={6} title="Ile średnio trwa Twój jeden trening?" />
-            <RadioQuestion
-              options={[
-                "Mniej niż 30 minut",
-                "Mniej niż godzinę",
-                "Więcej niż godzinę",
-                "Więcej niż dwie godziny",
-              ]}
-              name="howLong"
-              handleChange={(howLong: string) =>
-                dispach({
-                  type: formActionTypes.updateHowLong,
-                  payload: howLong,
-                })
-              }
-            />
-
-            <Title number={7} title="Gdzie najbardziej lubisz ćwiczyć?" />
-            <CheckBoxQuestion
-              options={[
-                "na świeżym powietrzu",
-                "na siłowni",
-                "w domu",
-                "jest to mi obojętne",
-              ]}
-              name="places"
-              value={places}
-              handleChange={(newPlace: string) => {
-                dispach({
-                  type: formActionTypes.updatePlaces,
-                  payload: newPlace,
-                });
-              }}
-            />
-            <Title
-              number={8}
-              title="Które aspekty uprawiania sportu są dla Ciebie najważniejsze?"
-            />
-            <CheckBoxQuestion
-              options={[
-                "możliwość zrzucenia wagi",
-                "możliwość wyrzeźbienia sylwetki",
-                "zachowanie zdrowia",
-                "poprawa kondycji",
-                "sposób na spędzenie wolnego czasu",
-                "sposób na relaks",
-                "możliwość poznania nowych ludzi",
-              ]}
-              name="mostImportant"
-              value={mostImportant}
-              handleChange={(newImportant: string) => {
-                dispach({
-                  type: formActionTypes.updateMostImportant,
-                  payload: newImportant,
-                });
-              }}
-            />
-            <Title
-              number={9}
-              title="Czy kiedykolwiek miałeś/aś kontuzję z powodu uprawiania sportu?"
-            />
-            <RadioQuestion
-              options={["Tak", "Nie"]}
-              name="injury"
-              handleChange={(injury: string) =>
-                dispach({
-                  type: formActionTypes.updateInjury,
-                  payload: injury,
-                })
-              }
-            />
-            <Title
-              number={10}
-              title="Czy kiedykolwiek brałeś udział w zawodach sportowych?"
-            />
-            <RadioQuestion
-              options={["Tak", "Nie"]}
-              name="competition"
-              handleChange={(competition: string) =>
-                dispach({
-                  type: formActionTypes.updateCompetition,
-                  payload: competition,
-                })
-              }
-            />
-          </form>
+          <Formik
+            enableReinitialize
+            initialValues={{
+              gender,
+              age,
+              isInterested,
+              whichSport,
+              howOften,
+              howLong,
+              places,
+              mostImportant,
+              injury,
+              competition,
+            }}
+            validationSchema={FormSchema}
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <Title number={1} title="Zaznacz swoją płeć" />
+                <RadioQuestion
+                  options={["Kobieta", "Mężczyzna"]}
+                  name="gender"
+                  handleChange={(gender: string) =>
+                    dispach({
+                      type: formActionTypes.updateGender,
+                      payload: gender,
+                    })
+                  }
+                />
+                {errors.gender && touched.gender ? (
+                  <div style={{ color: "red" }}>{errors.gender}</div>
+                ) : null}
+                <Title number={2} title="Ile masz lat?" />
+                <TextFieldQuestion
+                  type="number"
+                  name="age"
+                  value={age}
+                  handleChange={(age: string) =>
+                    dispach({
+                      type: formActionTypes.updateAge,
+                      payload: age,
+                    })
+                  }
+                />
+                {errors.age && touched.age ? (
+                  <div style={{ color: "red" }}>{errors.age}</div>
+                ) : null}
+                <Title number={3} title="Czy interesujesz się sportem?" />
+                <RadioQuestion
+                  options={[
+                    "Nie",
+                    "Tak, uprawiam sport",
+                    "Tak, jestem kibicem",
+                    "Tak, kibicuję i uprawiam sport",
+                  ]}
+                  name="isInterested"
+                  handleChange={(isInterested: string) =>
+                    dispach({
+                      type: formActionTypes.updateIsInterested,
+                      payload: isInterested,
+                    })
+                  }
+                />
+                {errors.isInterested && touched.isInterested ? (
+                  <div style={{ color: "red" }}>{errors.isInterested}</div>
+                ) : null}
+                <Title
+                  number={4}
+                  title="Które z wymienionych sportów uprawiasz najczęściej?"
+                />
+                <CheckBoxQuestion
+                  options={[
+                    "piłka nożna",
+                    "bieganie",
+                    "siatkówka",
+                    "lekkoatletyka",
+                    "piłka ręczna",
+                    "koszykówka",
+                    "gimnastyka artystyczna",
+                    "narciarstwo",
+                    "tenis",
+                    "pływanie",
+                    "łyżwiarstwo",
+                    "taniec",
+                  ]}
+                  name="whichSport"
+                  value={whichSport}
+                  handleChange={(newSport: string) => {
+                    dispach({
+                      type: formActionTypes.updateWhichSport,
+                      payload: newSport,
+                    });
+                  }}
+                />
+                {errors.whichSport && touched.whichSport ? (
+                  <div style={{ color: "red" }}>{errors.whichSport}</div>
+                ) : null}
+                <Title number={5} title="Jak często uprawiasz sport?" />
+                <RadioQuestion
+                  options={[
+                    "1 raz w miesiącu",
+                    "2 razy w miesiącu",
+                    "Raz w tygodniu",
+                    "Kilka razy w tygodniu",
+                    "Codziennie",
+                  ]}
+                  name="howOften"
+                  handleChange={(howOften: string) =>
+                    dispach({
+                      type: formActionTypes.updateHowOften,
+                      payload: howOften,
+                    })
+                  }
+                />
+                {errors.howOften && touched.howOften ? (
+                  <div style={{ color: "red" }}>{errors.howOften}</div>
+                ) : null}
+                <Title
+                  number={6}
+                  title="Ile średnio trwa Twój jeden trening?"
+                />
+                <RadioQuestion
+                  options={[
+                    "Mniej niż 30 minut",
+                    "Mniej niż godzinę",
+                    "Więcej niż godzinę",
+                    "Więcej niż dwie godziny",
+                  ]}
+                  name="howLong"
+                  handleChange={(howLong: string) =>
+                    dispach({
+                      type: formActionTypes.updateHowLong,
+                      payload: howLong,
+                    })
+                  }
+                />
+                {errors.howLong && touched.howLong ? (
+                  <div style={{ color: "red" }}>{errors.howLong}</div>
+                ) : null}
+                <Title number={7} title="Gdzie najbardziej lubisz ćwiczyć?" />
+                <CheckBoxQuestion
+                  options={[
+                    "na świeżym powietrzu",
+                    "na siłowni",
+                    "w domu",
+                    "jest to mi obojętne",
+                  ]}
+                  name="places"
+                  value={places}
+                  handleChange={(newPlace: string) => {
+                    dispach({
+                      type: formActionTypes.updatePlaces,
+                      payload: newPlace,
+                    });
+                  }}
+                />
+                {errors.places && touched.places ? (
+                  <div style={{ color: "red" }}>{errors.places}</div>
+                ) : null}
+                <Title
+                  number={8}
+                  title="Które aspekty uprawiania sportu są dla Ciebie najważniejsze?"
+                />
+                <CheckBoxQuestion
+                  options={[
+                    "możliwość zrzucenia wagi",
+                    "możliwość wyrzeźbienia sylwetki",
+                    "zachowanie zdrowia",
+                    "poprawa kondycji",
+                    "sposób na spędzenie wolnego czasu",
+                    "sposób na relaks",
+                    "możliwość poznania nowych ludzi",
+                  ]}
+                  name="mostImportant"
+                  value={mostImportant}
+                  handleChange={(newImportant: string) => {
+                    dispach({
+                      type: formActionTypes.updateMostImportant,
+                      payload: newImportant,
+                    });
+                  }}
+                />
+                {errors.mostImportant && touched.mostImportant ? (
+                  <div style={{ color: "red" }}>{errors.mostImportant}</div>
+                ) : null}
+                <Title
+                  number={9}
+                  title="Czy kiedykolwiek miałeś/aś kontuzję z powodu uprawiania sportu?"
+                />
+                <RadioQuestion
+                  options={["Tak", "Nie"]}
+                  name="injury"
+                  handleChange={(injury: string) =>
+                    dispach({
+                      type: formActionTypes.updateInjury,
+                      payload: injury,
+                    })
+                  }
+                />
+                {errors.injury && touched.injury ? (
+                  <div style={{ color: "red" }}>{errors.injury}</div>
+                ) : null}
+                <Title
+                  number={10}
+                  title="Czy kiedykolwiek brałeś udział w zawodach sportowych?"
+                />
+                <RadioQuestion
+                  options={["Tak", "Nie"]}
+                  name="competition"
+                  handleChange={(competition: string) =>
+                    dispach({
+                      type: formActionTypes.updateCompetition,
+                      payload: competition,
+                    })
+                  }
+                />
+                {errors.competition && touched.competition ? (
+                  <div style={{ color: "red" }}>{errors.competition}</div>
+                ) : null}
+                <div
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: "40%",
+                  }}
+                >
+                  <StyledButton text="OK" />
+                </div>
+              </Form>
+            )}
+          </Formik>
         </CardContent>
       </Card>
     </Container>
